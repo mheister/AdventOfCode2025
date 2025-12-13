bbb7193baf8f361dde83ad742fabe68b253c6cb3
Marcel Heistermann <mheistermann@gmx.de>
2025-12-13 16:50:01 +0100

    day07 part 2

--- day07/main.v
+++ day07/main.v
@@ -2,6 +2,7 @@ module main
 
 import arrays
 import os
+import math
 
 fn read_input(filename string) ![][]u8 {
 	data := os.read_file(filename) or { return error('Failed to read file: ${err}') }
@@ -19,7 +20,7 @@ fn read_input(filename string) ![][]u8 {
 fn print_manifold(m [][]u8) {
 	for l in m {
 		str := arrays.join_to_string[u8](l, '', |x| x.ascii_str())
-		println("${str}")
+		println('${str}')
 	}
 }
 
@@ -27,29 +28,35 @@ fn find_beam_indices(row []u8) []int {
 	mut indices := []int{cap: row.len}
 	for i, val in row {
 		match val {
-			u8(`S`) {indices << i}
-			u8(`|`) {indices << i}
+			u8(`S`) { indices << i }
+			u8(`|`) { indices << i }
 			else {}
 		}
 	}
 	return indices
 }
 
-fn shoot_beam(mut m [][]u8) u32 {
-	mut splits := u32(0)
-	for row in 1..m.len {
+fn shoot_beam(mut m [][]u8) (u64, u64) {
+	mut splits := u64(0)
+	mut multiplicities := [][]u64{len: m.len, init: []u64{len: m[0].len, init: 0}}
+	for row in 1 .. m.len {
 		beam_indices := find_beam_indices(m[row - 1])
 		for idx in beam_indices {
-			if m[row][idx] == u8(`.`) {
+			multiplicity := math.max(multiplicities[row - 1][idx], 1)
+			if m[row][idx] == u8(`.`) || m[row][idx] == u8(`|`) {
 				m[row][idx] = u8(`|`)
+				multiplicities[row][idx] += multiplicity
 			} else if m[row][idx] == u8(`^`) {
+				splits++
 				m[row][idx - 1] = u8(`|`)
 				m[row][idx + 1] = u8(`|`)
-				splits++
+				multiplicities[row][idx - 1] += multiplicity
+				multiplicities[row][idx + 1] += multiplicity
 			}
 		}
 	}
-	return splits
+	total_multiplicity := arrays.sum(multiplicities.last()) or { 0 }
+	return splits, total_multiplicity
 }
 
 fn main() {
@@ -58,9 +65,9 @@ fn main() {
 
 	print_manifold(input)
 	println('\n--- Shooting beam ---\n')
-	splits := shoot_beam(mut input)
+	splits, timelines := shoot_beam(mut input)
 	print_manifold(input)
 
 	println('Part 1: ${splits}')
-	println('Part 2: ')
+	println('Part 2: ${timelines}')
 }
